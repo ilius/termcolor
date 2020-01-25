c4c8d3dbe3f617125d4be19b2d09cfed4302dec0
Saeed Rasooli <saeed.gnu@gmail.com>
2020-01-25 23:40:15 +0800

    initial commit

---
+++ gen-colors.go
@@ -0,0 +1,87 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ilius/termcolor"
+)
+
+var values = [6]int{
+	0x00,
+	0x5f, // prev + 95
+	0x87, // prev + 40
+	0xaf, // prev + 40
+	0xd7, // prev + 40
+	0xff, // prev + 40
+}
+
+var colors = map[int][3]int{
+	0:  {0, 0, 0},       // #000000
+	1:  {204, 0, 0},     // #cc0000
+	2:  {78, 154, 6},    // #4e9a06
+	3:  {196, 160, 0},   // #c4a000
+	4:  {52, 101, 164},  // #3465a4
+	5:  {117, 80, 123},  // #75507b
+	6:  {6, 152, 154},   // #06989a
+	7:  {211, 215, 207}, // #d3d7cf
+	8:  {85, 87, 83},    // #555753
+	9:  {239, 41, 41},   // #ef2929
+	10: {138, 226, 52},  // #8ae234
+	11: {252, 233, 79},  // #fce94f
+	12: {114, 159, 207}, // #729fcf
+	13: {173, 127, 168}, // #ad7fa8
+	14: {52, 226, 226},  // #34e2e2
+	15: {238, 238, 236}, // #eeeeec
+}
+
+func getRGB(n int) (int, int, int) {
+	if n < 16 {
+		c := colors[n]
+		return c[0], c[1], c[2]
+	}
+	if n >= 232 {
+		v := 8 + 10*(n-232)
+		return v, v, v
+	}
+	m := n - 16
+
+	red := values[int(m/36)]
+	green := values[int((m%36)/6)]
+	blue := values[m%6]
+
+	return red, green, blue
+}
+
+func main() {
+	file, err := os.Create("colors-new.go")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fmt.Fprintf(file, `package termcolor
+
+import (
+	"image/color"
+)
+
+type ColorProp struct {
+	RGBA color.RGBA
+	Name string
+}
+
+var Colors = [256]*ColorProp{`)
+	for num, name := range termcolor.ColorNames {
+		red, green, blue := getRGB(num)
+		fmt.Fprintf(
+			file,
+			"\t{color.RGBA{0x%02x, 0x%02x, 0x%02x, 0xff}, %#v}, // %d \n",
+			red, green, blue,
+			name,
+			num,
+		)
+	}
+	fmt.Fprintf(file, "}\n")
+}
---
+++ gen-names.go
@@ -0,0 +1,23 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ilius/termcolor"
+)
+
+func main() {
+	file, err := os.Create("names.go")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fmt.Fprintf(file, "package termcolor\n\nvar ColorNames = []string{\n")
+	for _, c := range termcolor.Colors {
+		fmt.Fprintf(file, "\t%#v,\n", c.Name)
+	}
+	fmt.Fprintf(file, "}\n")
+}
