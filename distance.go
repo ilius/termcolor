7d459362cda11d3b559f251d8771eed04710fdc4
Saeed Rasooli <saeed.gnu@gmail.com>
2020-01-26 13:24:50 +0800

    add distance.go

---
+++ distance.go
@@ -0,0 +1,19 @@
+package termcolor
+
+import (
+	"image/color"
+	"math"
+)
+
+func chanDiffSq(x1 uint8, x2 uint8) int32 {
+	d := int32(x1) - int32(x2)
+	return d * d
+}
+
+func DistanceRGB(c1 color.RGBA, c2 color.RGBA) float64 {
+	return math.Sqrt(float64(
+		chanDiffSq(c1.R, c2.R) +
+			chanDiffSq(c1.G, c2.G) +
+			chanDiffSq(c1.B, c2.B),
+	))
+}
