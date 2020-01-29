2b01f7fd18e90930051bba8ad204a0eb16ec1ccd
Saeed Rasooli <saeed.gnu@gmail.com>
2020-01-30 03:14:25 +0800

    add ColorCodeByName in byname.go, generated by gen-byname.go

---
+++ gen-byname.go
@@ -0,0 +1,42 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"go/format"
+	"io/ioutil"
+
+	"github.com/ilius/termcolor"
+)
+
+var ignoreColors = map[int]bool{
+	16:  true, // duplicate of 0 = black
+	231: true, // duplicate of 15 = white
+}
+
+func main() {
+	goCode := `//go:generate go run gen-byname.go
+	
+package termcolor
+
+var ColorCodeByName = map[string]uint8{
+`
+	for code, c := range termcolor.Colors {
+		if ignoreColors[code] {
+			continue
+		}
+		for _, name := range c.Names {
+			goCode += fmt.Sprintf("\t%#v: %d,\n", name, code)
+		}
+	}
+	goCode += "}"
+	goCodeNew, err := format.Source([]byte(goCode))
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("byname.go", goCodeNew, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
