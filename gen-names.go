1d3873024911519c6d0ef5623d6ca57ad9487c73
Saeed Rasooli <saeed.gnu@gmail.com>
2020-02-02 17:48:01 +0800

    gen-names.go: format Go code before writing to file

--- gen-names.go
+++ gen-names.go
@@ -4,20 +4,24 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"go/format"
+	"io/ioutil"
 
 	"github.com/ilius/termcolor"
 )
 
 func main() {
-	file, err := os.Create("names.go")
+	goCode := "package termcolor\n\nvar ColorNames = [][]string{\n"
+	for _, c := range termcolor.Colors {
+		goCode += fmt.Sprintf("\t%#v,\n", c.Names)
+	}
+	goCode += "}"
+	goCodeNew, err := format.Source([]byte(goCode))
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	fmt.Fprintf(file, "package termcolor\n\nvar ColorNames = [][]string{\n")
-	for _, c := range termcolor.Colors {
-		fmt.Fprintf(file, "\t%#v,\n", c.Names)
+	err = ioutil.WriteFile("names.go", goCodeNew, 0644)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Fprintf(file, "}\n")
 }
