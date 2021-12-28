e271d5bb60833d9d5b52ffa842a1ef3027103cee
Saeed Rasooli <saeed.gnu@gmail.com>
2021-12-29 01:32:09 +0330

    add colors.md, gen-colors-markdown.go

---
+++ gen-colors-markdown.go
@@ -0,0 +1,70 @@
+//go:build ignore
+// +build ignore
+
+// reads names.go (var ColorNames) and generates colors.go
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/ilius/termcolor"
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+func uint8to32(x uint8) uint32 {
+	return uint32(float64(x) * 257.0)
+}
+
+type simpleRGB [3]uint8
+
+func (c simpleRGB) RGBA() (r, g, b, a uint32) {
+	return uint8to32(c[0]), uint8to32(c[1]), uint8to32(c[2]), 0xffff
+}
+
+func formatFloat(f float64) string {
+	return fmt.Sprintf(strings.TrimRight(strings.TrimRight(fmt.Sprintf("%.3f", f), "0"), "."))
+}
+
+func formatHSL(h, s, l float64) string {
+	return fmt.Sprintf(
+		"%s, %s, %s",
+		formatFloat(h),
+		formatFloat(s),
+		formatFloat(l),
+	)
+}
+
+func main() {
+	mdText := `## Terminal / ANSI Colors
+
+| Code | Hex       | RGB           | HSL               |
+| ---- | --------- | ------------- | ----------------- |
+`
+	for code, _ := range termcolor.ColorNames {
+		red, green, blue := termcolor.CodeToRGB(uint8(code))
+		cf, ok := colorful.MakeColor(simpleRGB{red, green, blue})
+		if !ok {
+			panic("failed to make color")
+		}
+		H, S, L := cf.Hsl()
+		hexColor := termcolor.RGBToHexColor(red, green, blue)
+		rgb := fmt.Sprintf("%d, %d, %d", red, green, blue)
+		hsl := formatHSL(H, S, L)
+		mdText += fmt.Sprintf(
+			"| %- 4s | `%- 7s` | %- 13s | %- 17s |\n",
+			fmt.Sprintf("%v", code),
+			hexColor,
+			rgb,
+			hsl,
+		)
+	}
+	{
+		err := ioutil.WriteFile("colors.md", []byte(mdText), 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
