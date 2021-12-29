2600f7e9cfdbafab1c4f3a20743af021f640667e
Saeed Rasooli <saeed.gnu@gmail.com>
2021-12-29 22:26:28 +0330

    update export-html.go

--- export-html.go
+++ export-html.go
@@ -18,9 +18,15 @@ func main() {
 	}
 	defer fp.Close()
 
+	colorColWidth := "%50"
+	codeColWidth := "%15"
+	hexColWidth := "%30"
+
+	// tableWidth := "260em"
 	// tableWidth := "%50"
-	tableWidth := "600px"
-	codeColWidth := "60px"
+	// tableWidth := "50vw"
+	tableWidth := "300px"
+
 	bgColor := "#252525"
 	fgColor := "#ffffff"
 
@@ -28,19 +34,19 @@ func main() {
 	fmt.Fprintf(fp, "<body style=\"background-color:%s;color:%s;\">\n", bgColor, fgColor)
 	fmt.Fprintf(fp, "<table width=\"%s\">\n", tableWidth)
 	fmt.Fprintf(fp, "\t<tr>\n\t\t"+strings.Join([]string{
-		`<th width="%%30">Color</th>`,
+		`<th width="%s" align="center">Color</th>`,
 		`<th width="%s" align="center">#</th>`,
-		`<th width="%%30">HTML</th>`,
-	}, "\n\t\t"), codeColWidth)
+		`<th width="%s" align="center">Hex</th>`,
+	}, "\n\t\t"), colorColWidth, codeColWidth, hexColWidth)
 
 	for code, c := range termcolor.Colors {
 		if code != int(c.Code) {
 			panic("bad color code")
 		}
 		fmt.Fprintf(fp, "\t<tr>\n")
-		fmt.Fprintf(fp, "\t\t"+`<td width="%%30" style="background-color:%s">&nbsp;</th>`, c.Hex)
-		fmt.Fprintf(fp, "\t\t"+`<td width="%s" align="center">%d</th>`, codeColWidth, code)
-		fmt.Fprintf(fp, "\t\t"+`<td width="%%30">%s</th>`, c.Hex)
+		fmt.Fprintf(fp, "\t\t"+`<td width="%s" style="background-color:%s">&nbsp;</td>`, colorColWidth, c.Hex)
+		fmt.Fprintf(fp, "\t\t"+`<td width="%s" align="center">%d</td>`, codeColWidth, code)
+		fmt.Fprintf(fp, "\t\t"+`<td width="%s"><pre>%s</pre></td>`, hexColWidth, c.Hex)
 	}
 
 	fmt.Fprintf(fp, "\n</table>\n</body>\n</html>")
